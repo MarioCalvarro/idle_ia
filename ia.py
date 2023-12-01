2f45d07af01aeec54b1cee200ce3d992b21f5dea
Mario <mcalvarr@ucm.es>
2023-12-01 11:16:10 +0100

    Created individual class

--- ia.py
+++ ia.py
@@ -13,16 +13,55 @@ import random
 #  * Mutación: Elegir un 10% de los segundos y sobre cada uno sumar o restar a cada mina una cantidad
 #              que dependa de su coste (las minas baratas tendrán más variabilidad)
 
-class IdleGeneticProblem(object):
+class IdleIndividual():
+    BASE_RANGE_MUTATION: int = 10
+
+    def __init__(self, seconds: int):
+        self.values: list[list[int]] = self.create_new_values(seconds)
+        self.fitness: int = -1
+
+    def create_new_values(self, seconds: int):
+        l: list[list[int]] = [[]] * seconds # Create list of `seconds` empty lists
+        for i in range(seconds):
+            for _ in range(6):     # 6 types of mines
+                number: int = random.randint(0, 1) * i * 15 // seconds   # Initially buy 0 to 2 mines max
+                l[i].append(number)
+        return l
+
+    def mutation(self):
+        number_mutations: int = len(self.values) // 10
+        to_mutate: list[int] = [] 
+        
+        for _ in range(number_mutations):
+            to_mutate.append(random.randint(0, len(self.values) - 1))
+        
+        for index in to_mutate:
+            self.mutate_index(index)
+
+    def mutate_index(self, index: int):
+        new_base_range_mutation: int = self.BASE_RANGE_MUTATION * index // 90 
+        #Six because we have 6 types of mines
+        for i in range(6):
+            new_base_range_mutation //= 2
+            prob: float = 1 / i * 0.5
+            if prob > random.uniform(0, 1):
+                self.values[index][i] += random.randint(-new_base_range_mutation, new_base_range_mutation)
+
+
+
+
+
+class IdleGeneticProblem():
         def __init__(self):
             """Constructor"""
                 
         def decodifica(self, genotype):
             """Return the phenotype given a genotype"""
 
-        def muta(self, individual, prob):
-            """Return a new individual from a previous one with 
-                a posible mutation"""   
+        def muta(self, individual: IdleIndividual):
+            """Return a new individual from a previous one with
+                a posible mutation"""
+            individual.mutation();                
 
         def cruza(self, individual1, individual2):         
             """Return the cross of two individuals"""
